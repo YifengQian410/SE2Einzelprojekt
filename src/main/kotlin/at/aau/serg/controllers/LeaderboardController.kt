ee6952994638f38e09ce13d9ea6e4324f6557f62
Yifeng Qian <yiqian@edu.aau.at>
2026-03-14 16:25:10 +0100

    Add missing imports for RequestParam and ResponseStatusException

--- src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
+++ src/main/kotlin/at/aau/serg/controllers/LeaderboardController.kt
@@ -5,6 +5,9 @@ import at.aau.serg.services.GameResultService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.server.ResponseStatusException
+import org.springframework.http.HttpStatus
 
 @RestController
 @RequestMapping("/leaderboard")
